07629436c2b2ddc8eabed187a299daeac5def278
wellitsnoor <wellitsnoor@gmail.com>
2025-05-27 17:51:39 +0530

    ts

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -2,14 +2,9 @@ import { notFound } from "next/navigation";
 import React from "react";
 import Image from "next/image";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default function Page({ params }: PageProps) {
-  if (params.id !== "1") {
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  if (id !== "1") {
     return notFound();
   }
 
